06e2dde0996476e4618cceb51174f3f80a7ee0ca
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-07-31 21:36:53 +0500

    Add sidebar button to Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
+import { FiAlignRight } from "react-icons/fi";
 
 const Navbar = () => {
     return (
@@ -11,6 +12,11 @@ const Navbar = () => {
                             <a href="/" className="text-white text-2xl font-bold">
                             <Image src={"/logo.jpg"} alt="Khyber Afghan English Language & Computer Academy" width={100} height={100} className='w-[80px] h-[80px] rounded-full border-2 border-blue-400' />
                             </a>
+                        </div>
+                        <div className="sidebarButton">
+                            <button className="bg-white rounded-md font-bold text-xl text-black p-2 hover:bg-blue-500 hover:text-white transition duration-300 ease-in-out">    
+                                <FiAlignRight />
+                            </button>
                         </div>
                     </div>
                 </div>
