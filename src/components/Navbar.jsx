b1318ed6d2318ce1fa1b617792cda51a442f6fe1
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-07-31 18:07:48 +0500

    Added Logo in navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Image from 'next/image'
+
+const Navbar = () => {
+    return (
+        <header className="w-full bg-blue-700">
+            <div className="container mx-auto">
+                <div className="header flex items-center justify-between py-2 px-1">
+                    <div className="logo-area flex items-center justify-between gap-5">
+                        <div className="logo">
+                            <a href="/" className="text-white text-2xl font-bold">
+                            <Image src={"/logo.jpg"} alt="Khyber Afghan English Language & Computer Academy" width={100} height={100} className='w-[80px] h-[80px] rounded-full border-2 border-blue-400' />
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar
