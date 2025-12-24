0b213e2e84bcae805aaacada3e7b44d4b73d27d2
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-12-24 03:27:11 -0800

    fix header layout issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ const Header = ({ }) => {
 
 
     return (
-        <header className="bg-blue-900 text-white dark:bg-slate-950 relative">
+        <header className="bg-blue-900 text-white dark:bg-slate-950 z-50 sticky top-1">
             <div className="container mx-auto flex items-end justify-between relative">
                
                {/* logo section  */}
@@ -25,7 +25,7 @@ const Header = ({ }) => {
                 <div className='user-data flex items-end justify-center m-2 gap-3 cursor-pointer relative' onClick={()=>setOpenedDailog(!openedDailog)}>
                     <p className="md:text-2xl text-xl">Israfeel Khan</p>
                     <span className='md:text-4xl text-3xl'><FaUser /></span>
-                    <div className={`absolute right-0 top-full bg-blue-900 dark:bg-slate-950 p-2 w-full transition-all duration-200 ${openedDailog?"h-[100px] opacity-100":"h-[0px] opacity-0 overflow-hidden"}`}>
+                    <div className={`absolute right-0 top-full z-50 bg-blue-900 dark:bg-slate-950 p-2 w-full transition-all duration-200 ${openedDailog?"h-[100px] opacity-100":"h-[0px] opacity-0 overflow-hidden"}`}>
                         <Link href="/profile" className="px-1 py-2 bg-gray-200 hover:bg-white text-black rounded-sm w-full block my-1">Profile</Link>
                         <Link href="/logout" className="px-1 py-2 bg-gray-200 hover:bg-white text-black rounded-sm w-full block my-1">Logout</Link>
                     </div>
