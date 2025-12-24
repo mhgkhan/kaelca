65ca495242114b1ae867ba918f47e087780a59b3
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-12-24 03:21:01 -0800

    Fixes Size of text in mobile mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,8 +23,8 @@ const Header = ({ }) => {
 
                 {/* nav section  */}
                 <div className='user-data flex items-end justify-center m-2 gap-3 cursor-pointer relative' onClick={()=>setOpenedDailog(!openedDailog)}>
-                    <p className="text-2xl">Israfeel Khan</p>
-                    <span className='text-4xl'><FaUser /></span>
+                    <p className="md:text-2xl text-xl">Israfeel Khan</p>
+                    <span className='md:text-4xl text-3xl'><FaUser /></span>
                     <div className={`absolute right-0 top-full bg-blue-900 dark:bg-slate-950 p-2 w-full transition-all duration-200 ${openedDailog?"h-[100px] opacity-100":"h-[0px] opacity-0 overflow-hidden"}`}>
                         <Link href="/profile" className="px-1 py-2 bg-gray-200 hover:bg-white text-black rounded-sm w-full block my-1">Profile</Link>
                         <Link href="/logout" className="px-1 py-2 bg-gray-200 hover:bg-white text-black rounded-sm w-full block my-1">Logout</Link>
