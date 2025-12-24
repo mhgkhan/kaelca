8ee6a907efb3fb9875e8094b406422da350250ec
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-12-24 03:18:39 -0800

    Add facicons and content metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -31,6 +31,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="KAELACA" />
+      </head>
       <body
         className={`${ruluko.className} ${merriweathersans.variable}  antialiased`}
       >
