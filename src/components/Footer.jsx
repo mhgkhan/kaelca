b8a022253b89efbdccd9705fe99413b1a8fe7296
Muhammad Hasnain Ghazna Khan <muhammadhasnainghazna@gmail.com>
2025-12-24 03:10:13 -0800

    fix footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import Logo from './Logo'
 
 const Footer = () => {
   return (
-    <footer className="bg-blue-900 text-white dark:bg-slate-950 relative mt-10">
+    <footer className="bg-blue-900 text-white dark:bg-slate-950 relative">
         <div className="container mx-auto flex flex-col gap-5 items-center justify-center p-4">
             <Logo />
             <p className="text-center">&copy; {new Date().getFullYear()} KHYBER AGHAN ENGLISH LANGUAGE AND COMPUTER ACADEMY. All rights reserved.</p>
